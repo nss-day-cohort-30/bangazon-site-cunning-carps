0dada05f0481b4c842f2eb1156e7f9ef41d08201
James Farrell <jamesfarrellmusic@gmail.com>
2019-06-03 13:56:03 -0700

    expiration date will validate but won't throw error message

---
+++ Bangazon/Migrations/20190603200847_paymentExpiryAdded.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bangazon.Migrations
+{
+    public partial class paymentExpiryAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExpiryDate",
+                table: "PaymentType",
+                maxLength: 7,
+                nullable: true);
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropColumn(
+                name: "ExpiryDate",
+                table: "PaymentType");
+        }
+    }
+}
